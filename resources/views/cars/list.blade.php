8d2ef2f32d7c73a9a6e17dfee4dee127e67d1e43
s087440@student.tu.kielce.pl <123Edcvbnm>
2020-03-26 21:24:31 +0100

    prototype of home page

--- resources/views/cars/list.blade.php
+++ resources/views/cars/list.blade.php
@@ -7,7 +7,20 @@
 @section('content')
 
     <div class="container">
+        <div class="row {{--align-items-center align-content-center align-self-center--}} {{--justify-content-center--}}">
 
-        cars list
+
+        @for($i=0; $i<89;$i++)
+            <div class="card {{--col-3 --}}m-1 @if($i % 5 == 0) bg-danger text-white @endif" style="width:32%">
+                <img class="card-img-top" {{--src="https://cdn.pixabay.com/photo/2016/04/01/09/11/car-1299198_960_720.png"--}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTDxzZex8EwEq_MQk7goFmsVgPyvBuEujerlM2nXh7a8Xhq8gle" alt="Card image"
+                     style="width:100%">
+                <div class="card-body">
+                    <h4 class="card-title">Moje ulubione auto</h4>
+                    <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                    <a href="https://www.google.com/" class="btn btn-primary stretched-link">See Profile</a>
+                </div>
+            </div>
+        @endfor
+    </div>
     </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{--{{ str_replace('_', '-', app()->getLocale()) }}--}}pl">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/cars') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -58,7 +58,12 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{-- __('Logout') --}} Zmień hasło i href
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{-- __('Logout') --}} Wyloguj
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
